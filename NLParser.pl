4e7a0d985c76b3a3ac555c5459af354da48b94c9
samsuite <suitester@gmail.com>
2017-12-03 19:44:27 -0500

    detecting moves! yeah baby

--- NLParser.pl
+++ NLParser.pl
@@ -1,14 +1,14 @@
-main(Index) :-
+:- use_module(mazeInfo, [info/3, wall/2, button/3, num_buttons/1, start/2, goal/2]).
+
+main() :-
     open('NL-input.txt', read, Str),
     read_file(Str,Lines),
     %Convert the lines in file to an list of sentences that are lists of words
     lines_to_words(Lines, Words),
 
-    nth0(Index, Words, Sent),
-
     write(Words), nl,
-    write(Sent), nl,
-    sentence(Sent),
+    check_sentences(Words),
+
     close(Str).
 
 % Credit to StackOverflow and author Ishq for file parser
@@ -29,6 +29,13 @@ lines_to_words([H|T], [H2|T2]) :-
 	lines_to_words(T, T2).
 
 
+check_sentences([]).
+
+check_sentences([S|Words]) :-
+    write(S),
+
+    ( sentence(S) -> valid_move(S) ; write(" INVALID") ), nl,
+    check_sentences(Words).
 
 
 
@@ -47,13 +54,20 @@ verb("scurried").
 verb("pushed").
 
 object("button").
+object("square").
 object("squares").
 object("cell").
+object("cells").
 
 direction("up").
 direction("down").
 direction("left").
 direction("right").
+
+up("up").
+down("down").
+left("left").
+right("right").
 
 
 subject_phrase(X, Y) :-
@@ -98,3 +112,56 @@ sentence([V, W, X, Y, Z]) :-
 sentence([U, V, W, X, Y, Z]) :-
     subject_phrase(U, V),
     verb_phrase(W, X, Y, Z).
+
+
+
+% "he pushed the button"
+valid_move([W, X, Y, Z]) :-
+    subject_phrase(W),
+    verb_phrase(X, Y, Z),
+    write("  BUTTON PUSH"), nl.
+
+% "the rat pushed the button"
+valid_move([V, W, X, Y, Z]) :-
+    subject_phrase(V, W),
+    verb_phrase(X, Y, Z),
+    write("  BUTTON PUSH"), nl.
+
+% "he moved 2 squares up"
+valid_move([V, W, X, Y, Z]) :-
+    subject_phrase(V),
+    verb_phrase(W, X, Y, Z),
+
+    number_codes(N, X),
+
+    write("  MOVE "),
+    write(N), nl,
+    attempt_walk(Z, N).
+
+% "the rat moved 2 squares up"
+valid_move([U, V, W, X, Y, Z]) :-
+    subject_phrase(U, V),
+    verb_phrase(W, X, Y, Z),
+
+    number_codes(N, X),
+    
+    write("  MOVE "), 
+    write(N), nl,
+    attempt_walk(Z, N).
+    
+
+
+attempt_walk(Direction, Number) :-
+    (   up(Direction) ->
+        writeln("UP")
+        
+    ;   down(Direction) ->
+        writeln("DOWN")
+
+    ;   left(Direction) ->
+        writeln("LEFT")
+
+    ;   right(Direction) ->
+        writeln("RIGHT")
+    ),
+    nl.
